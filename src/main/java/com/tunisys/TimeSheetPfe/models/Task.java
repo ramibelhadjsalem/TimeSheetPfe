149fcedfda349bea640316d0818ddb2fdf66f5b7
ramibelhadjsalem <rami.belhadj.salam@gmail.com>
2025-03-08 04:56:13 +0100

    task entity added

---
+++ src/main/java/com/tunisys/TimeSheetPfe/models/Task.java
@@ -0,0 +1,19 @@
+package com.tunisys.TimeSheetPfe.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+}
