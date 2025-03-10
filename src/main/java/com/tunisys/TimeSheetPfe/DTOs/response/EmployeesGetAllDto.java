3a62c90c3272ec355c3391891624822d52dd9980
ramibelhadjsalem <rami.belhadj.salam@gmail.com>
2025-03-10 08:21:14 +0100

    nn

--- src/main/java/com/tunisys/TimeSheetPfe/DTOs/response/EmployeesGetAllDto.java
+++ src/main/java/com/tunisys/TimeSheetPfe/DTOs/response/EmployeesGetAllDto.java
@@ -1,5 +1,6 @@
 package com.tunisys.TimeSheetPfe.DTOs.response;
 
+import com.tunisys.TimeSheetPfe.models.ERole;
 import com.tunisys.TimeSheetPfe.models.Project;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class EmployeesGetAllDto {
     private String phone;  // Employee Phone Number
     private String imgUrl;  // Employee Image URL (Optional)
     private List<TaskDto> tasks;
+    private List<Role> roles ;
 
 
 }
@@ -30,4 +32,10 @@ class TaskDto {
     private String status;  // Task Status
     private String priority;  // Task Priority
     private String difficulty;  // Task Difficulty
+}
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Role {
+    private ERole name;
 }
--- src/main/java/com/tunisys/TimeSheetPfe/controllers/EmployeesController.java
+++ src/main/java/com/tunisys/TimeSheetPfe/controllers/EmployeesController.java
@@ -1,22 +1,16 @@
 package com.tunisys.TimeSheetPfe.controllers;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import com.fasterxml.jackson.databind.type.CollectionLikeType;
 import com.tunisys.TimeSheetPfe.DTOs.response.EmployeesGetAllDto;
-import com.tunisys.TimeSheetPfe.DTOs.view.View;
 import com.tunisys.TimeSheetPfe.models.ERole;
 import com.tunisys.TimeSheetPfe.models.UserModel;
 import com.tunisys.TimeSheetPfe.services.userService.UserService;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
